98cefaa3904a351d10992389a4850886d4983edf
Jagannath Adhikary <jagannath.adhikary@sap.com>
2024-12-09 00:45:53 +0530

    add event in contract and config in off-chain code

--- ignition/modules/event-listener.js
+++ ignition/modules/event-listener.js
@@ -1,16 +1,26 @@
 const { ethers } = require("hardhat");
+const fs = require("fs");
+
 async function main() {
-    const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+    const contractAddress = config.contractAddress;
     const contractABI = require("../../artifacts/contracts/Auction.sol/Auction.json").abi;
 
-    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
+    const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
 
     const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
     console.log(`Listening to events from contract at: ${contractAddress}`);
-
+    contract.on("AuctionEndAttempt", (auctionId, blockTimestamp, auctionEndTime, event) => {
+        console.log(`
+            Auction end time: ${auctionEndTime}
+            Block time: ${blockTimestamp}
+            `);
+    });
     // Listen to events
     contract.on("AuctionCreated", (auctionId, creator, itemRef, startPrice, endTime, event) => {
+        const timeRemaining = endTime * 1000 - Date.now();
+        const thisAuctionId = auctionId;
         console.log(`
             Auction Created:
             ID: ${auctionId}
@@ -19,6 +29,17 @@ async function main() {
             Start Price: ${startPrice} ETH
             End Time: ${new Date(endTime * 1000).toLocaleString()}
         `);
+        setTimeout(async () => {
+            try {
+                const wallet = new ethers.Wallet(config.prividerPrivateAddress, provider);
+                const auctionContract = new ethers.Contract(contractAddress, contractABI, wallet);
+                const tx = await auctionContract.endAuction(thisAuctionId);
+                await tx.wait();
+                console.log(`Auction ${thisAuctionId} ended successfully`);
+            } catch(e) {
+                console.error(`Failed to end auction ${thisAuctionId}:`, e);
+            }
+        }, timeRemaining);
     });
 
     contract.on("BidPlaced", (auctionId, itemRef, bidder, amount, event) => {
